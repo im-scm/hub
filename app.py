ed3a511c55834986298074a859fa46d44d96e0d7
Marcelo Cardoso <marcelo.cardoso@impress.biz>
2026-06-15 14:00:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -539,10 +539,7 @@ filtered = create_safe_multiselect(filtered, "Supplier", "Supplier", numeric_no_
 filtered = create_safe_multiselect(filtered, "Currency", "Currency", numeric_no_decimal=False)
 filtered = create_safe_multiselect(filtered, "Lot (ton)", "Lot (ton)", numeric_no_decimal=True)
 
-st.write("")
-st.write("")
-st.write("")
-st.write("")
+st.sidebar.markdown("<br><br>", unsafe_allow_html=True)
 
 st.sidebar.markdown(
     f"""
